ebbacc617392a6f4581d86f7d1cc052548536578
MiguelAguas <MiguelAguas@users.noreply.github.com>
2021-10-20 22:05:14 +0200

    Add files via upload
    initial commit

---
+++ 01_hola.py
@@ -0,0 +1,28 @@
+print ("Hola")
+print ("Miguel")
+
+# Pseudocódigo:
+
+# nombre_variable = tipo_dato
+# imprime nombre_variable
+
+# inicializar variables una vez que las creas
+""" numero = 100
+decimal = 78.2
+cadena = "Hola"
+estado = True """
+numero = 100
+decimal = 78.2
+cadena = "Hola"
+estado = True
+
+# inicializar variables una vez que las creas
+
+print ("Tipos de Variables")
+print ("")
+print ("Variable de tipo entera: %d" %(numero))
+print ("Variable de tipo flotante: %f" %(decimal))
+print ("Variable de tipo cadena: %s" %(cadena))
+print ("Variable de tipo booleana: %d" %(estado))
+
+
