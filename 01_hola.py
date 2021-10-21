bb260d83f1f06a0f7700d81e407fd9ce486ec76d
MiguelAguas <MiguelAguas@users.noreply.github.com>
2021-10-21 02:58:14 +0200

    definir número decimales variable float
    definir número decimales variable float

--- 01_hola.py
+++ 01_hola.py
@@ -9,7 +9,7 @@ print ("Miguel")
 # inicializar variables una vez que las creas
 """ numero = 100
 decimal = 78.2
-cadena = "Hola"
+cadena = "Hola" 
 estado = True """
 numero = 100
 decimal = 78.2
@@ -21,7 +21,10 @@ estado = True
 print ("Tipos de Variables")
 print ("")
 print ("Variable de tipo entera: %d" %(numero))
+# si no especificamos el número de decimales, por defecto nos muestra 6
 print ("Variable de tipo flotante: %f" %(decimal))
+# podemos definir cuantos decimales queremos mostrar
+print ("Variable de tipo flotante con 2 decimales: %1.2f" %(decimal))
 print ("Variable de tipo cadena: %s" %(cadena))
 print ("Variable de tipo booleana: %d" %(estado))
 
